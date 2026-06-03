ca460db42741a62bc9025e64cd09bfcd99a1a63e
Vasim Katiya <vasimkatiya97@gmail.com>
2026-06-03 09:53:57 +0530

    cors bug fixing

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const cors = require('cors')
 const app = express();
 
 app.use(cors({
-  origin:"https://dev-folio83.netlify.app/",
+  origin:"https://dev-folio83.netlify.app",
   credentials:true
 }));
 
