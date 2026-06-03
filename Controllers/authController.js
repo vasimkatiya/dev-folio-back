302b48a7ee7a7a4c5218c90d7f436675fbaa2330
Vasim Katiya <vasimkatiya97@gmail.com>
2026-06-03 09:35:27 +0530

    deployment stage

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -50,7 +50,7 @@ exports.login = async (req, res) => {
         sameSite: "none"
         });
 
-        res.status(200).json({ message: 'Login successful', token });
+        res.status(200).json({ message: 'Login successful', token , username:user.username });
 
     } catch (error) {
         res.status(500).json({ message: 'Error logging in' });
